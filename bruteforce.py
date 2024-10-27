371d9eeb620afbd2680cd1495d43be04699f291d
PVL06 <pverl06@gmail.com>
2024-10-27 20:26:32 +0100

    csv in folder data, add optimized.py with numpy, move function get_data in file

--- bruteforce.py
+++ bruteforce.py
@@ -1,14 +1,13 @@
-import csv
-import os
 from typing import Generator
 
-FILE = "Liste_actions_P7.csv"
+from csv_reader import get_data
+
+FILE = "data/Liste_actions_P7.csv"
 MAX_INVEST = 500
 
-def get_data(path_file: str) -> list[(str, int, float)]:
-    with open(path_file, mode="r") as file:
-        reader = csv.reader(file)
-        return [(element[0], int(element[1]), int(element[2][:-1])/100 * int(element[1])) for element in list(reader)[1:]]
+"""
+O(n * 2^n)
+"""
 
 def get_combinations(n: int) -> Generator:
     for i in range(2**n - 1):
@@ -20,17 +19,17 @@ def get_best_combination(combinations: Generator, data: list[(str, int, float)])
     for combination in combinations:
         perf, invest= 0, 0
         for i, buy in enumerate(combination):
-            if int(buy):
+            if buy == "1":
                 invest += data[i][1]
                 perf += data[i][2]
 
         if invest <= MAX_INVEST and perf > best_perf:
             best_perf = perf
             best_combination = combination
-    return [data[index][0] for index, buy in enumerate(best_combination) if int(buy)]
+    return [data[index][0] for index, buy in enumerate(best_combination) if buy == "1"]
 
-file = f"{os.getcwd()}/{FILE}"
-data = get_data(file)
-combinations = get_combinations(len(data))
-best_combination = get_best_combination(combinations, data)
-print(best_combination)
+if __name__ == "__main__":
+    data = get_data(FILE)
+    combinations = get_combinations(len(data))
+    best_combination = get_best_combination(combinations, data)
+    print(best_combination)
