f14ee18de937523a4ded3e62e2ce00c274f03b83
PVL06 <pverl06@gmail.com>
2024-11-04 15:32:39 +0100

    add user input function

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 import csv
 from time import perf_counter
 
@@ -14,6 +15,27 @@ def get_data(path_file: str) -> list[tuple]:
         ]
 
 
+def user_selection():
+    csv_files = os.listdir("data/")
+    loop = True
+    while loop:
+        print("\nCsv file list: ")
+        for i, file in enumerate(csv_files):
+            print(f" {i} -> {file}")
+        choice = input("\nSelect csv file number: ")
+        invest = input("Enter investment (default=500): ")
+        try:
+            file = f"data/{csv_files[int(choice)]}"
+            max_invest = int(invest) if invest else 500
+        except ValueError:
+            print("Bad input! Enter a number")
+        except IndexError:
+            print("File number not in selection !")
+        else:
+            loop = False
+    return file, max_invest
+
+
 def time_exec(func):
     def wrapper(*args, **kwargs):
         start = perf_counter()
@@ -21,4 +43,3 @@ def time_exec(func):
         time_counter = (perf_counter() - start)*1000
         print(f"Execution time: {round(time_counter, 2)} ms\n")
     return wrapper
-    
