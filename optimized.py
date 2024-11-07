37762d37ee66a24833501589346ba0e949afa463
PVL06 <pverl06@gmail.com>
2024-11-07 10:50:23 +0100

    simplify display and get_best_shares, add comments

--- optimized.py
+++ optimized.py
@@ -2,8 +2,11 @@ import numpy as np
 
 from utils import get_data, time_exec, user_selection
 
-FACTOR = 100 # for number after the decimal point (1 -> 10, 2 -> 100) 
+FACTOR = 100 # for number after the decimal point (1 -> 10, 2 -> 100)
 
+
+# Create a 2D array with values from 0 to the maximum weight in columns
+# and calculate each row based on weight and value data
 def get_matrix(data: list[tuple], max_weight: int) -> np.array:
     matrix = np.zeros((len(data) + 1, max_weight + 1))
     for i in range(1, len(data) + 1):
@@ -13,9 +16,10 @@ def get_matrix(data: list[tuple], max_weight: int) -> np.array:
     return matrix
 
 
-def get_best_share(data: list[tuple], max_weight: int) -> list[str]:
+# Retrieve the best items from the matrix
+def get_best_shares(data: list[tuple], max_weight: int) -> list[tuple]:
     matrix = get_matrix(data, max_weight)
-    best_share = []
+    best_shares = []
     weight, index = 0, len(matrix)
     while weight < max_weight:
         column = matrix[:index, max_weight - weight]
@@ -23,20 +27,19 @@ def get_best_share(data: list[tuple], max_weight: int) -> list[str]:
             if column[i] != column[i-1]:
                 weight += data[i-1][1]
                 index = i
-                best_share.append(data[i-1][0])
+                best_shares.append(data[i-1])
                 break
             if i == 0:
                 weight = max_weight
-    return best_share
+    return best_shares
 
 
-def display_best_share(data: list[tuple], best_shares: list[str]) -> None:
+def display_best_shares(best_shares: list[tuple]) -> None:
     total_cost, performance = 0, 0
-    for share in data:
-        if share[0] in best_shares:
-            total_cost += share[1]
-            performance += share[2]
-            print(f"Buy {share[0]} - cost: {round(share[1]/FACTOR, 2)}, profit: {round(share[2],2 )}")
+    for share in best_shares:
+        print(f"Buy {share[0]} - cost: {round(share[1]/FACTOR, 2)}, profit: {round(share[2], 2)}")
+        total_cost += share[1]
+        performance += share[2]
     print("-"*50)
     print(f"Total cost: {round(total_cost/FACTOR, 2)}, profit: {round(performance, 2)}\n")
 
@@ -45,8 +48,8 @@ def display_best_share(data: list[tuple], best_shares: list[str]) -> None:
 def main(file: str, max_invest: int) -> None:
     data = get_data(file)
     data = [(share[0], int(share[1]*FACTOR), share[2]) for share in data]
-    best_share = get_best_share(data, max_invest*FACTOR)
-    display_best_share(data, best_share)
+    best_shares = get_best_shares(data, max_invest*FACTOR)
+    display_best_shares(data, best_shares)
 
 
 if __name__ == "__main__":
